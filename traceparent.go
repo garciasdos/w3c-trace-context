5aa874913f392def5c553887a4865d5b417b8d8f
Jan-Niklas Meier <jan@jansblog.org>
2022-04-23 14:17:40 +0200

    Adding forward compatibility traceparent parsing

--- traceparent.go
+++ traceparent.go
@@ -7,14 +7,24 @@ import (
 	"regexp"
 )
 
-const FlagSampled uint8 = 1
+const (
+	FlagSampled                         uint8 = 1
+	HighestSupportedTraceContextVersion       = 0
+)
 
 var (
-	traceIdFormat      = `[a-f0-9]{32}`
-	traceIdPattern     = regexp.MustCompile(`^` + traceIdFormat + `$`)
-	parentIdFormat     = `[a-f0-9]{16}`
-	parentIdPattern    = regexp.MustCompile(`^` + parentIdFormat + `$`)
-	traceParentPattern = regexp.MustCompile(`^[a-f0-9]{2}-` + traceIdFormat + `-` + parentIdFormat + `-[a-f0-9]{2}$`)
+	traceIdFormat          = `[a-f0-9]{32}`
+	traceIdPattern         = regexp.MustCompile(`^` + traceIdFormat + `$`)
+	traceIdAndDashPattern  = regexp.MustCompile(traceIdFormat + `-`)
+	parentIdFormat         = `[a-f0-9]{16}`
+	parentIdPattern        = regexp.MustCompile(`^` + parentIdFormat + `$`)
+	parentIdAndDashPattern = regexp.MustCompile(parentIdFormat + `-`)
+	versionFormat          = `^[a-f0-9]{2}-`
+	versionPattern         = regexp.MustCompile(versionFormat)
+	flagsFormat            = `[a-f0-9]{2}`
+	flagsPattern           = regexp.MustCompile(flagsFormat)
+	traceParentPattern     = regexp.MustCompile(
+		versionFormat + traceIdFormat + `-` + parentIdFormat + `-` + flagsFormat + `$`)
 )
 
 // TraceParent represents the information contained in the traceparent header
@@ -31,6 +41,24 @@ func ParseTraceParent(s string) (*TraceParent, error) {
 	parent := TraceParent{}
 
 	if !traceParentPattern.MatchString(s) {
+		// When the version prefix cannot be parsed (it's not 2 hex characters
+		// followed by a dash (-)), the implementation should restart the trace.
+		if !versionPattern.MatchString(s) {
+			return nil, errors.New("cannot parse traceparent version")
+		}
+
+		versionByte, err := hex.DecodeString(s[0:2])
+		if err != nil {
+			return nil, errors.New("cannot parse traceparent version")
+		}
+		parsedVersion := uint8(versionByte[0])
+
+		// If a higher version is detected, the implementation SHOULD try to
+		// parse it by trying the following
+		if parsedVersion > HighestSupportedTraceContextVersion {
+			return parseHigherVersion(s)
+		}
+
 		return nil, errors.New("traceparent doesn't match the specified pattern")
 	}
 
@@ -63,6 +91,60 @@ func ParseTraceParent(s string) (*TraceParent, error) {
 	parent.flags = parsedFlags[0]
 
 	return &parent, nil
+}
+
+// parseHigherVersion contains the logic to attempt to parse a traceparent that
+// has a version higher than 00.
+func parseHigherVersion(s string) (*TraceParent, error) {
+	// If the size of the header is shorter than 55 characters, the
+	// vendor should not parse the header and should restart the trace.
+	if len(s) < 55 {
+		return nil, errors.New("traceparent is shorter than 55 characters")
+	}
+
+	// Parse trace-id (from the first dash through the next 32 characters).
+	// Vendors MUST check that the 32 characters are hex, and that they are
+	// followed by a dash (-)
+	if !traceIdAndDashPattern.MatchString(s[3:37]) {
+		return nil, errors.New("cannot parse trace id")
+	}
+	traceId := s[3:35]
+
+	// Parse parent-id (from the second dash at the 35th position through the
+	// next 16 characters). Vendors MUST check that the 16 characters are hex
+	// and followed by a dash.
+	if !parentIdAndDashPattern.MatchString(s[36:53]) {
+		return nil, errors.New("cannot parse parent id")
+	}
+	parentId := s[36:52]
+
+	// Parse the sampled bit of flags (2 characters from the third dash).
+	if !flagsPattern.MatchString(s[53:55]) {
+		return nil, errors.New("cannot parse flags")
+	}
+	// Vendors MUST check that the 2 characters are either the end of the
+	// string or a dash.
+	if !(len(s) == 55 || (len(s) >= 56 && s[55] == '-')) {
+		return nil, errors.New("flags not followed by end of string or dash")
+	}
+
+	parsedFlags, err := hex.DecodeString(s[53:55])
+	if err != nil {
+		return nil, errors.New("cannot parse flags")
+	}
+	flags := parsedFlags[0]
+
+	// Vendors MUST use these fields to construct the new traceparent field
+	// according to the highest version of the specification known to the
+	// implementation (in this specification it is 00).
+	tp := TraceParent{
+		version:  HighestSupportedTraceContextVersion,
+		traceId:  traceId,
+		parentId: parentId,
+		flags:    flags,
+	}
+
+	return &tp, nil
 }
 
 // IsSampled returns true if the sampled flag in the TraceParent is set
@@ -125,5 +207,9 @@ func (tp *TraceParent) SetTraceId(traceId string) error {
 
 // String returns the string representation of the TraceParent
 func (tp *TraceParent) String() string {
-	return fmt.Sprintf("%02x-%s-%s-%02x", tp.version, tp.traceId, tp.parentId, tp.flags)
+	return fmt.Sprintf("%02x-%s-%s-%02x",
+		tp.version,
+		tp.traceId,
+		tp.parentId,
+		tp.flags)
 }
--- traceparent_test.go
+++ traceparent_test.go
@@ -28,19 +28,57 @@ func TestParseTraceParentZeroTraceParent(t *testing.T) {
 	}
 }
 
-func TestParseTraceParentNonZeroVersion(t *testing.T) {
-	_, err := ParseTraceParent("01-00000000000000000000000000000000-0000000000000000-00")
-
-	if err == nil {
-		t.Error("Parsed invalid version")
-	}
-}
-
 func TestParseTraceParentFFVersion(t *testing.T) {
 	_, err := ParseTraceParent("ff-0af7651916cd43dd8448eb211c80319c-00f067aa0ba902b7-01")
 
 	if err == nil {
 		t.Error("Incorrectly parsed ff version")
+	}
+}
+
+func TestParseTraceParentVersionOne(t *testing.T) {
+	tp, err := ParseTraceParent("01-0af7651916cd43dd8448eb211c80319c-00f067aa0ba902b7-01-123")
+
+	if err != nil {
+		t.Error("Could not parse valid future version:", err)
+	}
+	if tp.version != 1 {
+		t.Error("version not parsed correctly")
+	}
+	if tp.traceId != "0af7651916cd43dd8448eb211c80319c" {
+		t.Error("trace id not parsed correctly", tp.traceId)
+	}
+	if tp.parentId != "00f067aa0ba902b7" {
+		t.Error("parent id not parsed correctly")
+	}
+	if !tp.IsSampled() {
+		t.Error("sampled flag parsed correctly")
+	}
+
+	// Verify that version gets downgraded
+	s := tp.String()
+	newTp, err := ParseTraceParent(s)
+	if err != nil {
+		t.Error("Could not parse generated trace parent:", err)
+	}
+	if newTp.Version() != HighestSupportedTraceContextVersion {
+		t.Error("trace context version wasn't downgraded")
+	}
+}
+
+func TestParseTraceParentFutureVersionNotFollowingPattern(t *testing.T) {
+	_, err := ParseTraceParent("01-0af7651916cd43dd8448eb211c80319c1-00f067aa0ba902b7-01")
+
+	if err == nil {
+		t.Error("Incorrectly parsed invalid trace parent of future version")
+	}
+}
+
+func TestParseTraceParentFutureVersionTooLongFlags(t *testing.T) {
+	_, err := ParseTraceParent("01-0af7651916cd43dd8448eb211c80319c-00f067aa0ba902b7-010")
+
+	if err == nil {
+		t.Error("Incorrectly parsed invalid trace parent of future version")
 	}
 }
 
